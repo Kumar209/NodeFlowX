d6311e10a4909eb9eade1160564583cb2695914e
Kumar209 <prashantkumarlmp666@gmail.com>
2026-01-31 17:51:55 +0530

    added eventkey for inngest client for only production

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -22,6 +22,7 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events
 export const inngest = new Inngest({ 
     id: "nodeflowx",
-    // eventKey: process.env.INNGEST_EVENT_KEY,
+    // Only add eventKey in production
+  ...(process.env.NODE_ENV === "production" && { eventKey: process.env.INNGEST_EVENT_KEY }),
     middleware: [ realtimeMiddleware() ]
 });
